ee041a1eb326d5f8cb545204fc9ec13e7c4ae6b0
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2024-03-20 08:24:58 +0530

    Added Programming_Assignment1

--- Week09/Programming_Assignment1.py
+++ Week09/Programming_Assignment1.py
@@ -1,2 +1,15 @@
-# Coming Soon
-  	
+def balanced(word):
+    stack = []
+    for ch in word:
+        if ch == '(':
+            stack.append(')')
+        elif ch == '{':
+            stack.append('}')
+        elif ch == '[':
+            stack.append(']')
+        elif len(stack) == 0 or ch != stack[-1]:
+            return False
+        else:
+            stack.pop()
+            
+    return len(stack) == 0
