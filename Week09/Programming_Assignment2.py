59c2cfcaa1e9ca7483600af80287318e8635215a
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2024-03-20 08:25:43 +0530

    Added Programming_Assignment2

--- Week09/Programming_Assignment2.py
+++ Week09/Programming_Assignment2.py
@@ -1,1 +1,10 @@
-# Coming Soon
+def depth(exp):
+    count = 0
+    ans = 0
+    for ch in exp:
+        if ch in [ '(', '{', '[' ]:
+            count = count + 1
+        else:
+            ans = max(ans, count)
+            count = count - 1
+    return ans
