ddcb7427ac3cac046279c826cd6d5cedde41eec8
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2024-03-20 08:26:21 +0530

    Added Programming_Assignment3

--- Week09/Programming_Assignment3.py
+++ Week09/Programming_Assignment3.py
@@ -1,1 +1,14 @@
-# Coming Soon
+def matrix_multiply(A, B):
+    n = len(A)
+    result = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+  
+def power(A, m):
+    Axm = A
+    for i in range(1, m):
+        Axm = matrix_multiply(Axm, A)
+    return Axm
