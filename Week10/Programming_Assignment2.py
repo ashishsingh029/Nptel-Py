c62f56bd38dcb1ffd46fbaad4ef639fb26eb3eda
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2024-03-22 20:28:14 +0530

    Added Programming_Assignment2

--- Week10/Programming_Assignment2.py
+++ Week10/Programming_Assignment2.py
@@ -1,1 +1,14 @@
-# Coming Soon
+def is_magic(matrix):
+    n = len(matrix)
+    magic_sum = sum(matrix[0])
+    for i in range(n):
+        if sum(matrix[i]) != magic_sum:
+            return "NO"
+        if sum(matrix[j][i] for j in range(n)) != magic_sum:
+            return "NO"
+    if sum(matrix[i][i] for i in range(n)) != magic_sum:
+        return "NO"
+    if sum(matrix[i][n-i-1] for i in range(n)) != magic_sum:
+        return "NO"
+
+    return "YES"
