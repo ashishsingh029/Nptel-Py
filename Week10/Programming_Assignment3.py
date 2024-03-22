7852bc1da76ab0d4bf385bcee3481dd4b44df2cf
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2024-03-22 20:27:41 +0530

    Added Programming_Assignment3

--- Week10/Programming_Assignment3.py
+++ Week10/Programming_Assignment3.py
@@ -1,1 +1,9 @@
-# Coming Soon
+T = int(input())
+station_dict = {}
+for _ in range(T):
+    train_name = input()
+    station_dict[train_name] = {}
+    N = int(input())
+    for _ in range(N):
+        comaprtment, passengers = input().split(",")
+        station_dict[train_name][comaprtment] = int(passengers)
