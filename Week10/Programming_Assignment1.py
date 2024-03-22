bbdc58c2acccd605485c149e47aa488f61b9167b
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2024-03-22 20:00:38 +0530

    Added Programming_Assignment1

--- Week10/Programming_Assignment1.py
+++ Week10/Programming_Assignment1.py
@@ -1,1 +1,5 @@
-# Coming Soon
+def rotate(mat):
+    rows = len(mat)
+    cols = len(mat[0])
+    matTAndRev = [[mat[i][j] for i in range(rows-1, -1, -1)] for j in range(cols)]
+    return matTAndRev
